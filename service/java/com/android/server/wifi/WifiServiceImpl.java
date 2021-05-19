eb4b8d1c99a117eda34d479e3d633f64e4fb5bcf
Qiwei Cai <qcai@codeaurora.org>
2021-05-19 15:49:03 +0800

    wifi: Clear secondary STA info in factoryReset
    Remove network of secondary STA and clear related info
    when doing factory reset.
    
    CRs-Fixed: 2927268
    Change-Id: I8a5c4db115932880d75598be3ad29c39d34b2cc4

--- service/java/com/android/server/wifi/WifiServiceImpl.java
+++ service/java/com/android/server/wifi/WifiServiceImpl.java
@@ -3697,9 +3697,17 @@ public class WifiServiceImpl extends BaseWifiService {
         List<WifiConfiguration> networks = mWifiThreadRunner.call(
                 () -> mWifiConfigManager.getSavedNetworks(Process.WIFI_UID),
                 Collections.emptyList());
+
+        WifiConfigManager qtiWifiConfigManager = mWifiInjector.makeOrGetQtiWifiConfigManager();
+
+        networks.addAll(networks.size(), mWifiThreadRunner.call(
+                () ->qtiWifiConfigManager.getSavedNetworks(Process.WIFI_UID),
+                Collections.emptyList()));
+
         for (WifiConfiguration network : networks) {
             removeNetwork(network.networkId, packageName);
         }
+
         // Delete all Passpoint configurations
         List<PasspointConfiguration> configs = mWifiThreadRunner.call(
                 () -> mPasspointManager.getProviderConfigs(Process.WIFI_UID /* ignored */, true),
@@ -3707,10 +3715,22 @@ public class WifiServiceImpl extends BaseWifiService {
         for (PasspointConfiguration config : configs) {
             removePasspointConfigurationInternal(null, config.getUniqueId());
         }
+
+        mQtiPasspointManager = mWifiInjector.makeOrGetQtiPasspointManager();
+        configs = mWifiThreadRunner.call(
+                () -> mQtiPasspointManager.getProviderConfigs(
+                Process.WIFI_UID, true), Collections.emptyList());
+        for (PasspointConfiguration config : configs) {
+            removePasspointConfigurationInternal(null, config.getUniqueId(), STA_SECONDARY);
+        }
+
         mWifiThreadRunner.post(() -> {
             mPasspointManager.clearAnqpRequestsAndFlushCache();
+            mQtiPasspointManager.clearAnqpRequestsAndFlushCache();
             mWifiConfigManager.clearUserTemporarilyDisabledList();
             mWifiConfigManager.removeAllEphemeralOrPasspointConfiguredNetworks();
+            qtiWifiConfigManager.clearUserTemporarilyDisabledList();
+            qtiWifiConfigManager.removeAllEphemeralOrPasspointConfiguredNetworks();
             mClientModeImpl.clearNetworkRequestUserApprovedAccessPoints();
             mWifiNetworkSuggestionsManager.clear();
             mWifiInjector.getWifiScoreCard().clear();
